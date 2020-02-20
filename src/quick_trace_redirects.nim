0ed050d9cd7bfc76a13261786b61a7d510d2511b
Mark Baggett <mbagget1@utk.edu>
2020-02-20 14:42:21 -0500

    Refactor matching around creator names.

--- src/quick_trace_redirects.nim
+++ src/quick_trace_redirects.nim
@@ -6,6 +6,12 @@ type
     successes: seq[string]
     failures: seq[string]
 
+proc get_spaced_name(name: string): string =
+  if name != "":
+    return fmt" {name}"
+  else:
+    return name
+
 proc parse_data(response, element: string): seq[string] =
   let
     xml_response = Node.fromStringE(response)
@@ -27,7 +33,7 @@ proc get_new_records_from_digital_commons(oai_set: string): seq[string] =
 proc get_digital_commons_title_and_uris(digital_commons_records: seq[string]): seq[(string, string)] =
   for record in digital_commons_records:
     let
-      title = parse_data(record, "dc:title")[0]
+      title = parse_data(record, "dc:creator")[0]
       identifier = parse_data(record, "dc:identifier")[0]
     result.add((title, identifier))
 
@@ -36,9 +42,14 @@ proc get_islandora_etds(filename: string): seq[(string, string)] =
   p.open(filename, separator = '|')
   p.readHeaderRow()
   while p.readRow():
+    let
+      last_name = p.rowEntry("author1_lname")
+      first_name = p.rowEntry("author1_fname")
+      middle_name = p.rowEntry("author1_mname")
+      suffix = p.rowEntry("author1_suffix")
     result.add(
       (
-        p.rowEntry("title"),
+        fmt"{last_name}, {first_name}{get_spaced_name(middle_name)}{get_spaced_name(suffix)}",
         p.rowEntry("DELETE_original_uri_from_utk").replace("EMBARGOED OR DELETED: ", "").replace("/datastream/PDF", "")
       )
       )
